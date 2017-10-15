d9a8b3c9a140233927b727eb1eed9ad450b87a42
Tyler Hevia <tylerhevia@gmail.com>
2017-10-14 19:05:59 -0600

    install testing dependencies

---
+++ src/test/app.test.js
@@ -0,0 +1,9 @@
+const React = require("react");
+const { shallow, mount } = require("enzyme");
+const App = require("../components/App.tsx");
+
+describe("Tests", () => {
+  it("should run tests", () => {
+    expect(true).toEqual(true);
+  });
+});
