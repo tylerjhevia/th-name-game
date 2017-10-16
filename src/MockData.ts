69ffa935e3db8b56638fff630abf2edd8b5706cb
Tyler Hevia <tylerhevia@gmail.com>
2017-10-16 02:08:42 -0600

    add tests for Person component

---
+++ src/MockData.ts
@@ -0,0 +1,123 @@
+export const mockPeople = [
+  {
+    id: '4NCJTL13UkK0qEIAAcg4IQ',
+    type: 'people',
+    slug: 'joel-garrett',
+    jobTitle: 'Senior Software Engineer',
+    firstName: 'Joel',
+    lastName: 'Garrett',
+    headshot: {
+      type: 'image',
+      mimeType: 'image/jpeg',
+      id: '4Mv2CONANym46UwuuCIgK',
+      url:
+        '//images.contentful.com/3cttzl4i3k1h/4Mv2CONANym46UwuuCIgK/cbeb43c93a843a43c07b1de9954795e2/headshot_joel_garrett.jpg',
+      alt: 'headshot joel garrett',
+      height: 340,
+      width: 340
+    },
+    socialLinks: []
+  },
+  {
+    id: '1X2lomt8iIYImCQysey6Eq',
+    type: 'people',
+    slug: 'jeffery-ward',
+    jobTitle: 'Principal Software Engineer',
+    firstName: 'Jeff',
+    lastName: 'Ward',
+    headshot: {
+      type: 'image',
+      mimeType: 'image/jpeg',
+      id: '3BdQSQcuMgcs00qYoOuYSY',
+      url:
+        '//images.contentful.com/3cttzl4i3k1h/3BdQSQcuMgcs00qYoOuYSY/f0858540116928dc5fd0b2e84def8e19/headshot_jeff_ward.jpg',
+      alt: 'headshot jeff ward',
+      height: 340,
+      width: 340
+    },
+    socialLinks: []
+  },
+  {
+    id: '56zuFgdeoMqAOuIKe0M4AU',
+    type: 'people',
+    slug: 'ashley-joost',
+    jobTitle: 'Senior Software Engineer',
+    firstName: 'Ashley',
+    lastName: 'Joost',
+    headshot: {
+      type: 'image',
+      mimeType: 'image/jpeg',
+      id: 'ezBlWGiV9ucqAsiOo0Iy2',
+      url:
+        '//images.contentful.com/3cttzl4i3k1h/ezBlWGiV9ucqAsiOo0Iy2/01a8ed75dd4a508b45defea73fef6e80/headshot_ashley_joost.jpg',
+      alt: 'headshot ashley joost',
+      height: 340,
+      width: 340
+    },
+    socialLinks: []
+  },
+  {
+    id: '29XgxedqmAO86mGeImqwEK',
+    type: 'people',
+    slug: 'ben-frye',
+    jobTitle: 'Software Engineer',
+    firstName: 'Ben',
+    lastName: 'Frye',
+    headshot: {
+      type: 'image',
+      mimeType: 'image/jpeg',
+      id: '3SQLIq0Y36oYyaiwCSwOY8',
+      url:
+        '//images.contentful.com/3cttzl4i3k1h/3SQLIq0Y36oYyaiwCSwOY8/a65ae6620c8041b2773c1915156261d7/headshot_ben_frye.jpg',
+      alt: 'headshot ben frye',
+      height: 340,
+      width: 340
+    },
+    socialLinks: []
+  },
+  {
+    id: '2dMXmpIHPicQW6SW60qeKs',
+    type: 'people',
+    slug: 'christy-phillips',
+    jobTitle: 'Chief Talent Officer',
+    firstName: 'Christy',
+    lastName: 'Phillips',
+    headshot: {
+      type: 'image',
+      mimeType: 'image/jpeg',
+      id: '64IBagkE0gga82G2W6cWsm',
+      url:
+        '//images.contentful.com/3cttzl4i3k1h/64IBagkE0gga82G2W6cWsm/e3e89bed4184c6b36c34fe852b6edbf6/IMG_1035.jpg',
+      alt: 'Christy Phillips, Chief Talent Officer at WillowTree, Inc.',
+      height: 692,
+      width: 706
+    },
+    socialLinks: [
+      {
+        type: 'linkedin',
+        callToAction: 'Follow Christy Phillips on LinkedIn',
+        url: 'https://www.linkedin.com/in/christyphillips1/'
+      }
+    ]
+  }
+];
+
+export const mockPerson = {
+  id: '4NCJTL13UkK0qEIAAcg4IQ',
+  type: 'people',
+  slug: 'joel-garrett',
+  jobTitle: 'Senior Software Engineer',
+  firstName: 'Joel',
+  lastName: 'Garrett',
+  headshot: {
+    type: 'image',
+    mimeType: 'image/jpeg',
+    id: '4Mv2CONANym46UwuuCIgK',
+    url:
+      '//images.contentful.com/3cttzl4i3k1h/4Mv2CONANym46UwuuCIgK/cbeb43c93a843a43c07b1de9954795e2/headshot_joel_garrett.jpg',
+    alt: 'headshot joel garrett',
+    height: 340,
+    width: 340
+  },
+  socialLinks: []
+};
---
+++ src/Person.test.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Person } from './Person';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
+import { mockPerson } from './MockData';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const mockFn = jest.fn();
+
+describe('Person component', () => {
+  const wrapper = mount(
+    <Person
+      name={mockPerson.firstName}
+      headshot={mockPerson.headshot.url}
+      checkAnswer={mockFn}
+      reverseMode={false}
+    />
+  );
+
+  it('should receive the correct props', () => {
+    expect(wrapper.props().name).toEqual(mockPerson.firstName);
+    expect(wrapper.props().checkAnswer).toEqual(mockFn);
+    expect(wrapper.props().headshot).toEqual(mockPerson.headshot.url);
+    expect(wrapper.props().reverseMode).toEqual(false);
+  });
+
+  it('should render a div element with a class of "person"', () => {
+    expect(wrapper.find('div.person').length).toEqual(1);
+  });
+
+  it('should render an img element with a class of "headshot" when reverseMode is false', () => {
+    expect(wrapper.find('img.headshot').length).toEqual(1);
+  });
+
+  it('should render an h2 element with a class of "person-name" when reverseMode is true', () => {
+    const reverseWrapper = mount(
+      <Person
+        name={mockPerson.firstName}
+        headshot={mockPerson.headshot.url}
+        checkAnswer={mockFn}
+        reverseMode={true}
+      />
+    );
+    expect(reverseWrapper.props().reverseMode).toEqual(true);
+    expect(reverseWrapper.find('h2.person-name').length).toEqual(1);
+  });
+
+  it('should call the checkAnswer function when clicked', () => {
+    const person = wrapper.find('div.person');
+
+    expect(wrapper.props().checkAnswer).toHaveBeenCalledTimes(0);
+
+    person.simulate('click');
+
+    expect(wrapper.props().checkAnswer).toHaveBeenCalledTimes(1);
+  });
+});
